289c0f83ce9af72b60d30a9a6e3d580eaa8bdba3
Мансур <mowbnew@gmail.com>
2022-11-01 18:39:59 +0600

    merged all recipes/~ view sets into one RecipeViewSet

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,12 +8,12 @@ from .views import RecipeViewSet, SubscriptionViewSet, \
 app_name = 'api'
 
 router = routers.DefaultRouter()
-router.register(r'recipes/(?P<id>\d+)/favorite', FavoriteViewSet,
-                basename='favorites')
-router.register(r'recipes/(?P<id>\d+)/shopping_cart', ShoppingCartViewSet,
-                basename='favorites')
-router.register('recipes/download_shopping_cart', ShoppingCartViewSet,
-                basename='favorites')
+# router.register(r'recipes/(?P<id>\d+)/favorite', FavoriteViewSet,
+#                 basename='favorites')
+# router.register(r'recipes/(?P<id>\d+)/shopping_cart', ShoppingCartViewSet,
+#                 basename='shopping_carts')
+# router.register('recipes/download_shopping_cart', ShoppingCartViewSet,
+#                 basename='download_shopping_cart')
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('tags', TagViewSet, basename='tags')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
--- backend/api/views.py
+++ backend/api/views.py
@@ -6,6 +6,7 @@ from djoser.views import TokenDestroyView
 from rest_framework import status
 from rest_framework.authtoken import views
 from rest_framework.authtoken.models import Token
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ModelViewSet, mixins
@@ -18,6 +19,93 @@ from .serializers import UserSerializer, MeUserSerializer, RecipeSerializer, \
     RecipeCreateSerializer, TagSerializer, IngredientModelSerializer
 
 User = get_user_model()
+
+
+class RecipeViewSet(ModelViewSet):
+    queryset = Recipe.objects.all()
+    # serializer_class = RecipeSerializer
+    # filter_backends = (DjangoFilterBackend,)
+    # filterset_fields = ('id',)
+    lookup_field = 'id'
+
+    def get_serializer_class(self):
+        if self.action == ('list' or 'retrieve'):
+            return RecipeSerializer
+        return RecipeCreateSerializer
+
+    @action(methods=['GET'], detail=False, url_path='download_shopping_cart',
+            url_name='download_shopping_cart')
+    def download_shopping_cart(self, request, *args, **kwargs):
+        try:
+            shopping_cart = {}
+            ingredients = Recipe.objects.filter(
+                shopping_recipe__user=request.user
+            ).values_list(
+                'ingredients__ingredient__name',
+                'ingredients__ingredient__measurement_unit'
+            ).annotate(amount=Sum('ingredients__amount'))
+            for name, measurement_unit, amount in ingredients:
+                if name not in shopping_cart:
+                    shopping_cart[name] = {
+                        'measurement_unit': measurement_unit,
+                        'amount': amount
+                    }
+            file_text = ([f"> {item}: {value['amount']}"
+                          f"{value['measurement_unit']}\n"
+                          for item, value in shopping_cart.items()])
+            response = HttpResponse(file_text, 'Content-Type: text/plain')
+            response['Content-Disposition'] = (
+                f'attachment; '
+                f'filename="{self.request.user.username} shopping cart.txt"'
+            )
+            return response
+        except Exception as error:
+            return Response({'errors': str(error)},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    @action(methods=['POST', 'DELETE'], detail=True,
+            url_path='shopping_cart', url_name='shopping_carts')
+    def shopping_cart(self, request, *args, **kwargs):
+        try:
+            if self.request.method == 'POST':
+                recipe_id = self.kwargs.get('id')
+                recipe = Recipe.objects.get(pk=recipe_id)
+                user = User.objects.get(username=self.request.user)
+                ShoppingCart.objects.create(user=user, recipe=recipe)
+                serializer = FavoriteSerializer(recipe)
+                return Response(serializer.data,
+                                status=status.HTTP_201_CREATED)
+            # else only one available method DELETE
+            recipe_id = self.kwargs.get('id')
+            recipe = Recipe.objects.get(pk=recipe_id)
+            user = User.objects.get(username=self.request.user)
+            ShoppingCart.objects.get(user=user, recipe=recipe).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as error:
+            return Response({'errors': str(error)},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    @action(methods=['POST', 'DELETE'], detail=True, url_path='favorite',
+            url_name='favorites')
+    def favorite(self, request, *args, **kwargs):
+        try:
+            if self.request.method == 'POST':
+                recipe_id = self.kwargs.get('id')
+                recipe = Recipe.objects.get(pk=recipe_id)
+                user = User.objects.get(username=self.request.user)
+                Favorite.objects.create(user=user, recipe=recipe)
+                serializer = FavoriteSerializer(recipe)
+                return Response(serializer.data,
+                                status=status.HTTP_201_CREATED)
+            # else only one available method DELETE
+            recipe_id = self.kwargs.get('id')
+            recipe = Recipe.objects.get(pk=recipe_id)
+            user = User.objects.get(username=self.request.user)
+            Favorite.objects.get(user=user, recipe=recipe).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as error:
+            return Response({'errors': str(error)},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class SubscriptionViewSet(ModelViewSet):
@@ -41,49 +129,6 @@ class SubscriptionViewSet(ModelViewSet):
         Subscription.objects.get(user=self.request.user,
                                  author=user).delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class RecipeViewSet(ModelViewSet):
-    queryset = Recipe.objects.all()
-    # serializer_class = RecipeSerializer
-    # filter_backends = (DjangoFilterBackend,)
-    # filterset_fields = ('id',)
-    lookup_field = 'id'
-
-    def get_serializer_class(self):
-        if self.action == ('list' or 'retrieve'):
-            return RecipeSerializer
-        return RecipeCreateSerializer
-
-
-class FavoriteViewSet(ModelViewSet):
-    # queryset = Recipe.objects.all()
-    serializer_class = FavoriteSerializer
-    permission_classes = [IsAuthenticated]
-    pagination_class = None
-
-    def create(self, request, *args, **kwargs):
-        try:
-            recipe_id = self.kwargs.get('id')
-            recipe = Recipe.objects.get(pk=recipe_id)
-            user = User.objects.get(username=self.request.user)
-            Favorite.objects.create(user=user, recipe=recipe)
-            serializer = self.get_serializer(recipe)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        except Exception as error:
-            return Response({'errors': str(error)},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, *args, **kwargs):
-        try:
-            recipe_id = self.kwargs.get('id')
-            recipe = Recipe.objects.get(pk=recipe_id)
-            user = User.objects.get(username=self.request.user)
-            Favorite.objects.get(user=user, recipe=recipe).delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except Exception as error:
-            return Response({'errors': str(error)},
-                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginViewSet(views.ObtainAuthToken):
@@ -118,60 +163,3 @@ class IngredientViewSet(mixins.RetrieveModelMixin,
     queryset = Ingredient.objects.all()
     serializer_class = IngredientModelSerializer
     lookup_field = 'id'
-
-
-class ShoppingCartViewSet(ModelViewSet):
-    queryset = ShoppingCart.objects.all()
-    serializer_class = FavoriteSerializer
-    permission_classes = [AllowAny]
-
-    def list(self, request, *args, **kwargs):
-        try:
-            shopping_cart = {}
-            ingredients = Recipe.objects.filter(
-                shopping_recipe__user=request.user
-            ).values_list(
-                'ingredients__ingredient__name',
-                'ingredients__ingredient__measurement_unit'
-            ).annotate(amount=Sum('ingredients__amount'))
-            for name, measurement_unit, amount in ingredients:
-                if name not in shopping_cart:
-                    shopping_cart[name] = {
-                        'measurement_unit': measurement_unit,
-                        'amount': amount
-                    }
-            file_text = ([f"> {item}: {value['amount']}"
-                          f"{value['measurement_unit']}\n"
-                          for item, value in shopping_cart.items()])
-            response = HttpResponse(file_text, 'Content-Type: text/plain')
-            response['Content-Disposition'] = (
-                f'attachment; '
-                f'filename="{self.request.user.username} shopping cart.txt"'
-            )
-            return response
-        except Exception as error:
-            return Response({'errors': str(error)},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-    def create(self, request, *args, **kwargs):
-        try:
-            recipe_id = self.kwargs.get('id')
-            recipe = Recipe.objects.get(pk=recipe_id)
-            user = User.objects.get(username=self.request.user)
-            ShoppingCart.objects.create(user=user, recipe=recipe)
-            serializer = self.get_serializer(recipe)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        except Exception as err:
-            return Response({'errors': str(err)},
-                            status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, *args, **kwargs):
-        try:
-            recipe_id = self.kwargs.get('id')
-            recipe = Recipe.objects.get(pk=recipe_id)
-            user = User.objects.get(username=self.request.user)
-            ShoppingCart.objects.get(user=user, recipe=recipe).delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except Exception as error:
-            return Response({'errors': str(error)},
-                            status=status.HTTP_400_BAD_REQUEST)
