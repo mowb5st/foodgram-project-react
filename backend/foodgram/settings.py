eae0720e49682f04e4876010103030eca627e558
Мансур <mowbnew@gmail.com>
2022-11-15 12:27:05 +0600

    workflow fix: removed docker-compose down from workflow script; added STATIC_ROOT attr in setting instead if STATICFILES_DIRS

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -95,7 +95,8 @@ USE_TZ = True
 AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
