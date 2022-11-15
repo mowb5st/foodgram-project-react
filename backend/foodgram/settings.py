4ded42a341078681884a559ffd3667636fc0b3ed
Мансур <mowbnew@gmail.com>
2022-11-16 01:35:24 +0600

    workflow fix: try to fix static and media files

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, )
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 
 MEDIA_URL = '/media/'
